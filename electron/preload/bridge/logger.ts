22de6296849703e46e2caa0965e754d6e6894b44
Coke <jock74608@gmail.com>
2025-06-06 19:53:07 +0800

    refactor:electron: refactor electron dir.

--- electron/preload/bridge/logger.ts
+++ electron/preload/bridge/logger.ts
@@ -12,7 +12,6 @@ const consoleMethodMap = {
 type LogLevel = keyof typeof consoleMethodMap
 
 export const loggerBridge: Record<LogLevel, (...args: unknown[]) => void> = {} as never
-
 ;(Object.keys(consoleMethodMap) as LogLevel[]).forEach((level) => {
   loggerBridge[level] = (...args: unknown[]) => {
     ipcRenderer.send("log", level, ...args)
